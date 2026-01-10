36e49190ac4665556bb92a5cb314f7db532f5cc0
sreerajtr <trsreeraj07@gmail.com>
2026-01-10 16:02:52 +0530

    modified

--- config.php
+++ config.php
@@ -4,7 +4,7 @@
 // =====================================
 
 // Backend Base URL (Render)
-define('BACKEND_URL', 'https://mystore-production-8eef.up.railway.app');
+define('BACKEND_URL', 'https://mystore-backend-gk8t.onrender.com');
 
 // Backend API Base URL
 define('API_BASE_URL', BACKEND_URL . '/api/');
